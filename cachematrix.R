57e3c2d25002a17b7e7a626241a81bfafbaa4bd3
Ashleigh Keene <ashleighkeene@gmail.com>
2015-03-22 15:04:11 -0500

    version 1 of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix efficiently calculates the inverse of a matrix
+## by caching the original matrix and its inverse in a 
+## separate environment so that R can simply grab the solution
+## (the inverse matrix) from the cache rather than calculating
+# it over and over again.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix caches a matrix and its inverse in an 
+## environment outside the Global Environment and returns a 
+## list of functions: set (matrix), get (matrix), setinverse,
+## getinverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        nr <- nrow(x)
+        nc <- ncol(x)
+        matinv <- matrix(data=NULL, nrow=nr, ncol=ncol)
+        #print(environment())
+        #evn <- environment()
+        #print(parent.env(evn))
+        
+        set    <- function(y = matrix()) {
+                mat    <<- y
+                matinv <<- matrix(data=NULL, nrow=nr, ncol=ncol)
+        }
+        get    <- function() mat
+        setinv <- function(solve) matinv <<- solve
+        getinv <- function() matinv
+        getevn <- function() environment()
+        list(set    = set, 
+             get    = get,
+             setinv = setinv,
+             getinv = getinv,
+             getevn = getevn)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+#cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+#}
